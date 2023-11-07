e44e0325f493f2a632b96e62a6e7badaad38eca4
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-07 10:25:59 +0900

    feat: create message, textarea components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,8 @@
 export { default as Button } from './button';
 export { default as Input } from './input';
+export { default as FloatingButton } from './floating-button';
+export { default as Post } from './post';
 export { default as Layout } from './layout';
+export { default as Message } from './message';
+export { default as TextArea } from './textarea';
+export { default as Divider } from './divider';
---
+++ app/components/textarea.tsx
@@ -0,0 +1,36 @@
+import type { UseFormRegisterReturn } from 'react-hook-form';
+
+interface TextAreaProps {
+  label?: string;
+  name?: string;
+  register?: UseFormRegisterReturn;
+  required: boolean;
+  placeholder?: string;
+}
+
+export default function TextArea({
+  label,
+  name,
+  register,
+  ...rest
+}: TextAreaProps) {
+  return (
+    <section>
+      {label ? (
+        <label
+          htmlFor={name}
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
+          {label}
+        </label>
+      ) : null}
+      <textarea
+        {...register}
+        id={name}
+        className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
+        rows={4}
+        {...rest}
+      />
+    </section>
+  );
+}
