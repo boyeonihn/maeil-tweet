9e2717c62de43528b33fa53ca9630d814fdccf22
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-07 10:31:05 +0900

    feat: create session.ts for session/auth cookie storage

---
+++ app/lib/server/session.ts
@@ -0,0 +1,61 @@
+import {
+  IronSessionOptions,
+  getIronSession,
+  IronSessionData,
+  getServerActionIronSession,
+} from 'iron-session';
+
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+// session object options
+export const sessionOptions: IronSessionOptions = {
+  password: process.env.COOKIE_PW!,
+  cookieName: 'session-ck',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+};
+
+// auth-user object options (storing which user requested the token)
+export const authOptions: IronSessionOptions = {
+  password: process.env.COOKIE_PW!,
+  cookieName: 'auth-ck',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+};
+
+declare module 'iron-session' {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+// API route handler that uses getIronSession and returns response that needs to be called from client-side
+const getSession = async (req: NextRequest, res: NextResponse) => {
+  const session = getIronSession<IronSessionData>(req, res, sessionOptions);
+  return session;
+};
+
+// uses cookies() function from next/headers to set cookies so that Iron session can be used in nextJS server actions and react server components
+const getServerActionSession = async () => {
+  const session = getServerActionIronSession<IronSessionData>(
+    sessionOptions,
+    cookies()
+  );
+  return session;
+};
+
+const getServerActionAuthInfo = async () => {
+  const auth = getServerActionIronSession<IronSessionData>(
+    authOptions,
+    cookies()
+  );
+
+  return auth;
+};
+
+export { getSession, getServerActionSession, getServerActionAuthInfo };
