e8db2ef4ee730fdbd316faed8e13671cd25e16d3
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-07 10:40:09 +0900

    feat: build users/me api for current logged in user

---
+++ app/api/users/me/route.ts
@@ -0,0 +1,28 @@
+import { readCookieFromStorageServerAction } from '@/lib/server/serverActions';
+import prismaClient from '@/lib/server/prismaClient';
+import { NextResponse } from 'next/server';
+
+export const GET = async (isPrivate = true) => {
+  // bring logged in user's unique cookies
+  const cookieFromStorage = await readCookieFromStorageServerAction('session');
+
+  // console.log('api/users/me - checking cookies of logged in user', cookieFromStorage);
+  if (isPrivate && !cookieFromStorage.user) {
+    return NextResponse.json(
+      { ok: false, error: 'Please login!' },
+      { status: 401 }
+    );
+  }
+
+  const userProfile = await prismaClient.user.findUnique({
+    where: { id: cookieFromStorage.user?.id },
+  });
+
+  const userInfo = JSON.parse(
+    JSON.stringify(
+      userProfile,
+      (key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
+    )
+  );
+  return NextResponse.json({ ok: true, userInfo }, { status: 200 });
+};
