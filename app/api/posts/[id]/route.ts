9209c400109e39e3a6df810ff1327446c9c5ea60
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-07 14:20:49 +0900

    feat: create posts/id api

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prismaClient from '@/lib/server/prismaClient';
+import { readCookieFromStorageServerAction } from '@/lib/server/serverActions';
+
+export const GET = async (
+  req: NextRequest,
+  { params: { id } }: { params: { id: string } }
+) => {
+  const {
+    user: { id: userId },
+  } = await readCookieFromStorageServerAction('session');
+  const post = await prismaClient.post.findUnique({
+    where: {
+      id: +id,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  const keywords = post?.content.split(' ').map((keyword) => ({
+    content: {
+      contains: keyword,
+    },
+  }));
+
+  const relatedPosts = await prismaClient.post.findMany({
+    where: {
+      OR: keywords,
+      AND: {
+        id: {
+          not: +id,
+        },
+      },
+    },
+  });
+
+  const isLiked = await prismaClient.like.findFirst({
+    where: {
+      postId: post?.id,
+      userId,
+    },
+  });
+
+  return NextResponse.json(
+    { ok: true, post, relatedPosts, isLiked: !!isLiked },
+    { status: 200 }
+  );
+};
