e97e089d8dacf94473fe8a2e61a09d90a32dbd81
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 18:43:45 +0900

    feat: implement loading component

--- app/(routes)/tweets/[id]/page.tsx
+++ app/(routes)/tweets/[id]/page.tsx
@@ -70,8 +70,9 @@ const PostDetail = () => {
           </>
         ) : (
           <div className="flex justify-center items-center">
+            <span className="loading loading-lg loading-spinner text-primary"></span>
           </div>
-        </section>
+        )}
       </main>
     </Layout>
   );
