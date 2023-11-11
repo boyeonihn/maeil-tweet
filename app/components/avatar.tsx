5d57c85efd16cecbc8dd0e7766f9864637dd9a93
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 22:26:25 +0900

    feat: modify className of Avatar

--- app/components/avatar.tsx
+++ app/components/avatar.tsx
@@ -1,14 +1,24 @@
+import { cls } from '@/lib/client/utils';
+
 interface AvatarProps {
+  large?: boolean;
   initial: string;
   color?: 'primary' | 'secondary' | 'accent' | 'neutral';
 }
-export const Avatar = ({ initial, color = 'neutral' }: AvatarProps) => {
+export const Avatar = ({
+  large = false,
+  initial,
+  color = 'neutral',
+}: AvatarProps) => {
   return (
     <div className="avatar placeholder">
       <div
-        className={`bg-${color} bg-neutral-focus text-neutral-content rounded-full w-16 h-16`}
+        className={cls(
+          `bg-${color} bg-neutral-focus text-neutral-content rounded-full`,
+          large ? 'w-24 h-24' : 'w-16 h-16'
+        )}
       >
-        <span className="text-xl">{initial}</span>
+        <span className={large ? 'text-3xl' : 'text-xl'}>{initial}</span>
       </div>
     </div>
   );
