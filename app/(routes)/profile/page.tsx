122466228650d61fde3a4a0989035edbe35b6fb0
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 22:28:21 +0900

    feat: build UI of profile component

--- app/(routes)/profile/page.tsx
+++ app/(routes)/profile/page.tsx
@@ -1,9 +1,33 @@
-import { Layout } from '@/components';
+'use client';
+
+import useSWR from 'swr';
+import { Avatar, Layout, TextContainer } from '@/components';
+import { useUser } from '@/lib/client/hooks';
+import { getInitial, grabUserName } from '@/lib/client/utils';
+import { API_PATH } from '@/lib/const';
+import { PostsResponse } from '@/lib/types';
 
 export default function Profile() {
+  const { user, isLoading } = useUser();
+  const { data, isLoading: isLoadingData } = useSWR<PostsResponse>(
+    API_PATH.MYPOST
+  );
+  console.log('user in profile', user);
+
   return (
     <Layout title="My Profile" canGoBack hasTabBar>
-      <main></main>
+      <main className="py-10 flex flex-col justify-center space-y-6">
+        {user && (
+          <>
+            <div className="flex flex-col justify-center items-center space-y-3">
+              <Avatar large initial={getInitial(user.name)} />
+              <h1 className="text-4xl">{user.name}</h1>
+              <h2 className="text-2xl">@{grabUserName(user.email)}</h2>
+            </div>
+            {data && <TextContainer title="My Posts" postData={data.posts} />}
+          </>
+        )}
+      </main>
     </Layout>
   );
 }
