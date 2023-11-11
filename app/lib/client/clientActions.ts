3d10125e698ad3dd350cd4642bda015ead765570
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 16:55:49 +0900

    chore: replace api path with const variable

--- app/lib/client/clientActions.ts
+++ app/lib/client/clientActions.ts
@@ -1,5 +1,7 @@
 'use client';
 
+import { API_PATH } from '@/lib/const';
+
 export const submitCookieToStorageRouteHandler = async (cookie: string) => {
   console.log('submitting cookie to storage 🍊🍊🍊🍊🍊🍊');
   await fetch('/api/submitIronSessionCookie', {
@@ -15,15 +17,12 @@ export const submitCookieToStorageRouteHandler = async (cookie: string) => {
 
 export const readCookieFromStorageRouteHandler = async (): Promise<string> => {
   console.log('reading cookie from storage 🍊🍊🍊🍊🍊🍊');
-  const responseWithCookieFromStorage = await fetch(
-    '/api/readIronSessionCookie',
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  const responseWithCookieFromStorage = await fetch(API_PATH.ME, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
   const data = await responseWithCookieFromStorage.json();
   const cookieValue = data?.cookieInStorage || 'No Cookie In Storage';
   return cookieValue;
