526b6493a51d3be5e40d1308c9763bccd182fd15
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 13:49:50 +0900

    feat: create extractDateTime fn

--- app/lib/client/utils.ts
+++ app/lib/client/utils.ts
@@ -1,3 +1,12 @@
 export function cls(...classnames: string[]) {
   return classnames.join(' ');
 }
+
+export function extractDateTime(date: Date) {
+  const stringDate = date.toString();
+
+  const yymmdd = stringDate.slice(0, 10);
+  let time = stringDate.slice(11, 16);
+
+  return `${yymmdd} @ ${time}`;
+}
