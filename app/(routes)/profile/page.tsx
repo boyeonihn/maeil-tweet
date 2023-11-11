743f4109d3cb25e19ab28f4c6513fe83372e1d50
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 17:13:46 +0900

    feat: create profile component

---
+++ app/(routes)/profile/page.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '@/components';
+
+export default function Profile() {
+  return (
+    <Layout title="My Profile" canGoBack hasTabBar>
+      <main></main>
+    </Layout>
+  );
+}
