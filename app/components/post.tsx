cc77b73e4635d81aed4415614dc4c6e256bef4e8
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 13:49:36 +0900

    feat: simplify code with use of components

--- app/components/post.tsx
+++ app/components/post.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { Avatar } from './avatar';
+import { ReactionBtn } from './buttons/reactionButton';
 
 interface PostProps {
   content: string;
@@ -18,51 +20,21 @@ export default function Post({
   return (
     <Link
       href={`/tweets/${id}`}
-      className="flex px-4 pt-5 cursor-pointer justify-between"
+      className="flex pt-5 cursor-pointer justify-between"
     >
-      <div className="flex space-x-4">
-        <div className="w-20 h-20 bg-gray-400 rounded-md" />
-        <div className="pt-2 flex flex-col">
-          <h3 className="text-sm font-medium text-gray-900">{content}</h3>
-          <span className="font-medium mt-1 text-gray-900">{author}</span>
+      <section className="flex space-x-4 text-xl">
+        <Avatar initial={author.slice(0, 2).toUpperCase()} />
+        <div className="flex flex-col">
+          <div className="pt-2 flex flex-col">
+            <span className="font-medium mt-1">{author}</span>
+            <h3 className="font-medium">{content}</h3>
+          </div>
+          <div className="flex space-x-4">
+            <ReactionBtn btnType="like" hearts={hearts} />
+            <ReactionBtn btnType="comment" comments={comments} />
+          </div>
         </div>
-      </div>
-      <div className="flex space-x-2 items-end justify-end">
-        <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
-          <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-            ></path>
-          </svg>
-          <span>{hearts}</span>
-        </div>
-        <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
-          <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-            ></path>
-          </svg>
-          <span>{comments}</span>
-        </div>
-      </div>
+      </section>
     </Link>
   );
 }
