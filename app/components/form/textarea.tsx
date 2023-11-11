5163b6d79ff91b601fd45eb555ea2648305489b8
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 13:48:36 +0900

    chore: move input comp to form folder

---
+++ app/components/form/textarea.tsx
@@ -0,0 +1,70 @@
+import { cls } from '@/lib/client/utils';
+import type { UseFormRegisterReturn } from 'react-hook-form';
+import { Avatar } from '../avatar';
+
+interface TextAreaProps {
+  label?: string;
+  name?: string;
+  register?: UseFormRegisterReturn;
+  required: boolean;
+  placeholder?: string;
+  type: 'post' | 'reply';
+  colorType?: 'primary' | 'secondary' | 'accent';
+}
+
+export default function TextArea({
+  type = 'post',
+  label,
+  name,
+  register,
+  colorType = 'primary',
+  ...rest
+}: TextAreaProps) {
+  if (type === 'post') {
+    return (
+      <section className="form-control text-3xl">
+        {label ? (
+          <label htmlFor={name} className="label mb-1 block font-medium">
+            {<span className="label-text text-lg">{label}</span>}
+          </label>
+        ) : null}
+        <textarea
+          {...register}
+          id={name}
+          placeholder={label}
+          className={cls(
+            `textarea h-24 mt-1 shadow-md w-full rounded-md bg-primary bg-opacity-60 text-xl`,
+            colorType === 'secondary'
+              ? 'textarea-secondary'
+              : colorType === 'accent'
+              ? 'textarea-accent'
+              : 'textarea-primary'
+          )}
+          rows={4}
+          {...rest}
+        />
+      </section>
+    );
+  } else {
+    return (
+      <section className="form-control text-3xl">
+        <Avatar initial={'JO'} />
+        <textarea
+          {...register}
+          id={name}
+          placeholder={label}
+          className={cls(
+            `textarea h-24 mt-1 shadow-md w-full rounded-md bg-primary bg-opacity-60 text-xl`,
+            colorType === 'secondary'
+              ? 'textarea-secondary'
+              : colorType === 'accent'
+              ? 'textarea-accent'
+              : 'textarea-primary'
+          )}
+          rows={4}
+          {...rest}
+        />
+      </section>
+    );
+  }
+}
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,10 +1,10 @@
-export { default as Button } from './button';
-export { default as Input } from './input';
-export { default as FloatingButton } from './floating-button';
+export { default as Button } from './buttons/button';
+export { default as Input } from './form/input';
+export { default as FloatingButton } from './buttons/floating-button';
 export { default as Post } from './post';
 export { default as Layout } from './layout';
 export { default as Message } from './message';
-export { default as TextArea } from './textarea';
+export { default as TextArea } from './form/textarea';
 export { default as Divider } from './divider';
-export { default as WriteForm } from './writeForm';
-export { default as LogoutBtn } from './logoutBtn';
+export { default as WriteForm } from './form/writeForm';
+export { default as LogoutBtn } from './buttons/logoutBtn';
