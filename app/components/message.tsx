a87adcf109103fd76f78986d2e6df26cfebe3600
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-11 22:29:11 +0900

    style: update message styling

--- app/components/message.tsx
+++ app/components/message.tsx
@@ -18,7 +18,6 @@ export default function Message({
         reversed ? 'flex-row-reverse space-x-reverse' : 'space-x-2'
       )}
     >
-      <div className="w-8 h-8 rounded-full bg-slate-400" />
       <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
         <p>{message}</p>
       </div>
