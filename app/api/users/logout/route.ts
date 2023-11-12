8d9864fec1a81846a086c88581c2755754148f99
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-12 22:02:54 +0900

    feat: update logout function

--- app/api/users/logout/route.ts
+++ app/api/users/logout/route.ts
@@ -1,17 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
-import {
-  getServerActionAuthInfo,
-  getServerActionSession,
-} from '@/lib/server/session';
+import { getServerActionSession } from '@/lib/server/session';
 
 export const POST = async (req: NextRequest) => {
   const session = await getServerActionSession();
-  const auth = await getServerActionAuthInfo();
 
   console.log('logout attempts');
   if (!session) return;
   await session.destroy();
-  await auth.destroy();
 
   return NextResponse.json({ ok: true }, { status: 200 });
 };
