cc2dbbcae1462477525e2446cafb90913dd77674
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-12 19:06:30 +0900

    feat: implement dark/light mode

--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -3,6 +3,7 @@ import { cls } from '@/lib/client/utils';
 import React from 'react';
 import { Nav } from './nav';
 import { Header } from './header';
+import { SwitchTheme } from './switchTheme';
 
 interface LayoutProps {
   title?: string;
@@ -21,6 +22,9 @@ export default function Layout({
     <div className="flex flex-col items-center">
       <Header title={title} canGoBack={canGoBack} />
       <div className={cls('pt-12 w-full', hasTabBar ? 'pb-24' : '')}>
+        <div className="flex items-center justify-center pt-5">
+          <SwitchTheme />
+        </div>
         {children}
       </div>
       {hasTabBar ? <Nav /> : null}
