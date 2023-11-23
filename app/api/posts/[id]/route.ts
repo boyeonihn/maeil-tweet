6ad4d52a6ddaa345a0fe1341974abb53a5932ed1
Boyeon Ihn <boyeonihn@gmail.com>
2023-11-23 22:30:10 +0900

    refactor: separate keyword function to libs

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -11,70 +11,7 @@ export const GET = async (
     user: { id: userId },
   } = await readCookieFromStorageServerAction(SESSION);
 
-  const post = await prismaClient.post.findUnique({
-    where: {
-      id: +id,
-    },
-    include: {
-      user: {
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          avatar: true,
-        },
-      },
-    },
-  });
-
-  const comments = await prismaClient.comment.findMany({
-    where: {
-      post: {
-        id: +id,
-      },
-    },
-    include: {
-      user: {
-        select: {
-          name: true,
-          email: true,
-          avatar: true,
-        },
-      },
-    },
-  });
-
-  const keywords = post?.content.split(' ').map((keyword) => ({
-    content: {
-      contains: keyword,
-    },
-  }));
-
-  const relatedPosts = await prismaClient.post.findMany({
-    where: {
-      OR: keywords,
-      AND: {
-        id: {
-          not: +id,
-        },
-      },
-    },
-    include: {
-      _count: {
-        select: {
-          likes: true,
-          comments: true,
-        },
-      },
-      user: {
-        select: {
-          id: true,
-          name: true,
-          avatar: true,
-        },
-      },
-    },
-  });
+  const keywords = getKeywords(post?.content!);
 
   const isLiked = await prismaClient.like.findFirst({
     where: {
